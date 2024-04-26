aa03a8906d9dbfcfee9d08dd8332dd392dab1b6b
turtle <codingjinjin@gmail.com>
2024-04-26 15:56:41 -0500

    adjust container size, add audio and play buttons

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 
 
+const playIcon = document.querySelector("#playIcon");
+const music = document.querySelector("#music");
+
+playIcon.addEventListener("click", function() {
+    if (music.paused) {
+        music.play();
+        playIcon.src = "images/pauseicon.png"; 
+    } else {
+        music.pause();
+        playIcon.src = "images/playicon.png";
+    }
+});
 
 
 const gridContainer = document.querySelector('.gridContainer');
-const count = 560;
+const count = 448;
 for(let i = 0; i < count; i++) {
     let box = document.createElement('div'); 
     box.className = 'box'; 
@@ -19,3 +31,4 @@ for(let i = 0; i < box.length; i++) {
         box[i].style.animationDuration = 1 + duration + 's';
     });
 }
+
