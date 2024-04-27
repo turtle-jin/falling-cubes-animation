b10ef3bd2776784f3e4854efc5d7045ad2b6f180
turtle-jin <codingjinjin@gmail.com>
2024-04-26 22:09:19 -0500

    add mediaquery to make it mobile responsive

--- app.js
+++ app.js
@@ -15,7 +15,24 @@ playIcon.addEventListener("click", function() {
 
 
 const gridContainer = document.querySelector('.gridContainer');
-const count = 448;
+
+// Get computed style
+const computedStyle = window.getComputedStyle(gridContainer);
+
+
+
+// Retrieve css value 
+let width = parseInt(computedStyle.getPropertyValue("width"));
+console.log(`grid width is ${width}`)
+
+let count; 
+if(width === 1400) {
+    count = 448; 
+}else if (width === 350) {
+    count = 196;
+}
+
+
 for(let i = 0; i < count; i++) {
     let box = document.createElement('div'); 
     box.className = 'box'; 
