6682a9d79dd2518e3513f6f61cec361bf3b0f004
turtle-jin <codingjinjin@gmail.com>
2024-04-26 23:08:44 -0500

    remove song and player code for copyright reasons

--- app.js
+++ app.js
@@ -1,17 +1,6 @@
 
 
-const playIcon = document.querySelector("#playIcon");
-const music = document.querySelector("#music");
 
-playIcon.addEventListener("click", function() {
-    if (music.paused) {
-        music.play();
-        playIcon.src = "images/pauseicon.png"; 
-    } else {
-        music.pause();
-        playIcon.src = "images/playicon.png";
-    }
-});
 
 
 const gridContainer = document.querySelector('.gridContainer');
